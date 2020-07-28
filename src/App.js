ad72dfebce6edd8796c1c38cdd2787e99526bfc2
Victor Higaki <higvic@gmail.com>
2020-07-28 20:06:06 -0300

    Setup inicial do projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Menu from './components/Menu';
+import dados_iniciais from './data/dados_iniciais.json'
+import BannerMain from './components/BannerMain';
+import Footer from './components/Footer';
+import Carousel from './components/Carousel';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ background: "#141414" }}>
+      <Menu />
+
+      <BannerMain videoTitle={dados_iniciais.categorias[0].videos[0].titulo}
+        url={dados_iniciais.categorias[0].videos[0].url}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dados_iniciais.categorias[0]}
+      />
+
+      <Carousel category={dados_iniciais.categorias[1]}
+      />
+
+      <Carousel category={dados_iniciais.categorias[2]}
+      />
+
+      <Carousel category={dados_iniciais.categorias[3]}
+      />
+
+      <Carousel category={dados_iniciais.categorias[4]}
+      />
+
+      <Carousel category={dados_iniciais.categorias[5]}
+      />
+
+      <Footer />
     </div>
   );
 }
